47233f982ab8c3b6d0ddf96bbe1d971ec70d3f77
Joon1313 <wkdrud13@gmail.com>
2021-11-22 17:20:11 +0900

    init

--- src/DTO/user.findOne.dto.ts
+++
@@ -1,5 +0,0 @@
-export class UserFindOneDTO 
-{
-    
-    id:string
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm'
-import { User } from './entities/user.entity';
-import { UsersController } from './users/users.controller';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    "type": "postgres",
-    "host": "localhost",
-    "port": 5432,
-    "username": "postgres",
-    "password": "1313",
-    "database": "Nestjs",
-    "entities": [User],
-    "synchronize": true
-  }), UsersModule, AuthModule],
+
 })
 export class AppModule {
   constructor(private connection: Connection) {}
