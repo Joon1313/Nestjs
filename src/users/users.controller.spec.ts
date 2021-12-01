455c8e21661d4e42f4c4ae21dc0bab1fa608a292
Joon1313 <wkdrud13@gmail.com>
2021-12-01 21:34:20 +0900

    unit testing...1

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,20 +1,28 @@
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [JwtModule],
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [UsersService, JwtService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findOne', () => {
+    it('find user id', async () => {
+      const userId = ['test'];
+      // jest.spyOn(service, 'findOne').mockImplementation(() => userId);
+      expect(await controller.findOne(20)).toEqual('finded user name joon');
+    });
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,5 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
+
+class MockRepository {
+  async findOne(id) {
+    const user: User = new User();
+    user.id = id;
+    return user;
+  }
+}
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -7,12 +17,18 @@ describe('UsersService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UsersService],
+      // {
+      //   provide: getRepositoryToken(User),
+      //   useClass: MockRepository,
+      // },
     }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('findOne user', () => {
+    const id = 20;
+    const res = service.findOne(id);
+    expect(res).toEqual('finded user name joon');
   });
 });
